0131ed21c3f4e9cb84f015c9261f7e3937a3bcb1
Stephan Janssen <sja@devoxx.com>
2021-11-09 08:58:41 +0100

    Introduced ClockService

---
+++ src/main/java/org/janssen/scoreboard/service/ClockService.java
@@ -0,0 +1,129 @@
+package org.janssen.scoreboard.service;
+
+import org.janssen.scoreboard.controller.GameClockController;
+import org.janssen.scoreboard.model.Game;
+import org.janssen.scoreboard.model.type.GameType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import static org.janssen.scoreboard.service.util.Constants.FOUR_MINUTES;
+import static org.janssen.scoreboard.service.util.Constants.TEN_MINUTES_IN_SECONDS;
+
+@Service
+public class ClockService {
+
+    private final Logger log = LoggerFactory.getLogger(ClockService.class);
+
+    final private GameClockController gameClockController;
+    final private GameService gameService;
+
+    public ClockService(final GameClockController gameClockController,
+                        final GameService gameService) {
+        this.gameClockController = gameClockController;
+        this.gameService = gameService;
+    }
+
+    /**
+     * Start game clock.
+     * @param game  the game
+     * @return true when successfu
+     */
+    public boolean start(Game game) {
+        log.debug("Start game clock {}", game);
+
+        if (gameClockController.isRunning()) {
+            if (game.getClock() > 0 && gameClockController.getSeconds() > 0) {
+                gameClockController.start(game.getClock(), game.getGameType(), game.isMirrored());
+                return true;
+            } else {
+                log.info("Start clock but is 0");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Stop game clock.
+     * @param game  the game
+     * @return true when successful
+     */
+    public boolean stop(Game game) {
+        if (gameClockController.isRunning()) {
+
+            gameClockController.stop();
+            game.setClock(gameClockController.getSeconds());
+
+            gameService.update(game);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Start the pre-game count down clock.
+     *
+     * @param seconds   seconds to count down
+     * @param mirrored  mirror count down on seconds score board
+     */
+    public void startCountdown(int seconds, boolean mirrored) {
+        log.debug("Start game clock countdown {} secs and is mirrored {}", seconds, mirrored);
+
+        if (gameClockController.isRunning()) {
+            gameClockController.stop();
+        }
+
+        gameClockController.start(seconds, GameType.BASKET, mirrored);
+    }
+
+    /**
+     * Increment the game clock with x seconds.
+     * @param game  the game
+     * @param seconds   the total seconds to increment
+     * @return true when successful
+     */
+    public boolean incrementClock(Game game, int seconds) {
+        if (gameClockController.isRunning()) {
+            return false;
+        }
+
+        Integer clock = game.getClock();
+        if (clock < TEN_MINUTES_IN_SECONDS) {
+            if (game.getGameType() == GameType.BASKET_KIDS && clock >= FOUR_MINUTES) {
+                return false;
+            }
+            clock += seconds;
+            game.setClock(clock);
+            gameService.update(game);
+
+            gameClockController.setSeconds(clock);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Decrement the game clock with x seconds.
+     * @param game  the game
+     * @param seconds   the total seconds to decrement
+     * @return true  when successful
+     */
+    public boolean decrementClock(Game game, int seconds) {
+        if (gameClockController.isRunning()) {
+            return false;
+        }
+
+        Integer clock = game.getClock();
+        if (clock - seconds >= 0) {
+            clock -= seconds;
+            game.setClock(clock);
+            gameService.update(game);
+
+            gameClockController.setSeconds(clock);
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/org/janssen/scoreboard/web/rest/ClockResource.java
+++ src/main/java/org/janssen/scoreboard/web/rest/ClockResource.java
@@ -1,7 +1,7 @@
 package org.janssen.scoreboard.web.rest;
 
 import org.janssen.scoreboard.controller.GameClockController;
-import org.janssen.scoreboard.model.type.GameType;
+import org.janssen.scoreboard.service.ClockService;
 import org.janssen.scoreboard.service.GameService;
 import org.janssen.scoreboard.service.util.ResponseUtil;
 import org.slf4j.Logger;
@@ -10,9 +10,6 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import static org.janssen.scoreboard.service.util.Constants.FOUR_MINUTES;
-import static org.janssen.scoreboard.service.util.Constants.TEN_MINUTES_IN_SECONDS;
 
 /**
  * The clock service
@@ -26,17 +23,19 @@ public class ClockResource {
     private final Logger log = LoggerFactory.getLogger(ClockResource.class);
 
     private final GameService gameService;
+    private final ClockService clockService;
     private final GameClockController gameClockController;
 
     public ClockResource(GameService gameService,
+                         ClockService clockService,
                          GameClockController gameClockController) {
         this.gameService = gameService;
+        this.clockService = clockService;
         this.gameClockController = gameClockController;
     }
 
     @GetMapping("{gameId}")
-    public ResponseEntity<?> getClock(
-            @PathVariable("gameId") Long gameId) {
+    public ResponseEntity<?> getClock(@PathVariable("gameId") Long gameId) {
 
         log.debug("Get game clock");
 
@@ -54,43 +53,33 @@ public class ClockResource {
     }
 
     @PutMapping("/start/{gameId}")
-    public ResponseEntity<Object> startClock(@PathVariable("gameId") Long gameId) {
+    public ResponseEntity<?> startClock(@PathVariable("gameId") Long gameId) {
 
         log.debug("Start clock for game with id {} ", gameId);
 
-        if (gameClockController.isNotRunning()) {
-
-            if (gameId == null || gameId == 0) {
-                String msg = String.format("Wedstrijd ID (%d) is verkeerd, start 'New Game'.", gameId);
-                log.info(msg);
-                return ResponseEntity.badRequest().body(msg);
-            }
-
-            return gameService
-                    .findGameById(gameId)
-                    .map(game -> {
-                        if (game.getClock() > 0 && gameClockController.getSeconds() > 0) {
-                            gameClockController.start(game.getClock(), game.getGameType(), game.isMirrored());
-                        } else {
-                            log.info("Start clock but is 0");
-                        }
-                        return ResponseUtil.ok();
-                    })
-                    .orElse(ResponseUtil.badRequest("Game not found"));
+        if (gameId == null || gameId == 0) {
+            String msg = String.format("Wedstrijd ID (%d) is verkeerd, start 'New Game'.", gameId);
+            log.info(msg);
+            return ResponseEntity.badRequest().body(msg);
         }
-        return ResponseEntity.badRequest().body("Clock is running");
+
+        return gameService
+                .findGameById(gameId)
+                .map(game -> {
+                    if (clockService.start(game)) {
+                        return ResponseUtil.ok();
+                    } else {
+                        return ResponseUtil.badRequest();
+                    }
+                })
+                .orElse(ResponseUtil.badRequest("Game not found"));
     }
 
     @PutMapping("/countdown/{seconds}")
     public ResponseEntity<?> countDownClock(
             @PathVariable("seconds") int seconds,
             @RequestParam("mirrored") boolean mirrored) {
-
-        if (gameClockController.isRunning()) {
-            gameClockController.stop();
-        }
-
-        gameClockController.start(seconds, GameType.BASKET, mirrored);
+        clockService.startCountdown(seconds, mirrored);
         return ResponseUtil.ok();
     }
 
@@ -99,26 +88,21 @@ public class ClockResource {
 
         log.debug("Stop clock for game with id {} ", gameId);
 
-        if (gameClockController.isRunning()) {
-
-            if (gameId == null || gameId == 0) {
-                String msg = String.format("Wedstrijd ID (%d) is verkeerd, start 'New Game'.", gameId);
-                log.info(msg);
-                return ResponseEntity.badRequest().body(msg);
-            }
-
-            gameClockController.stop();
-
-            return gameService
-                .findGameById(gameId)
-                .map(game -> {
-                    game.setClock(gameClockController.getSeconds());
-                    gameService.update(game);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.badRequest().body("Wedstrijd niet gevonden, start 'New Game"));
+        if (gameId == null || gameId == 0) {
+            String msg = String.format("Wedstrijd ID (%d) is verkeerd, start 'New Game'.", gameId);
+            log.info(msg);
+            return ResponseEntity.badRequest().body(msg);
         }
 
-        return ResponseEntity.badRequest().body("Klok is niet actief, dus kan je ook niet stoppen");
+        return gameService
+                .findGameById(gameId)
+                .map(game -> {
+                    if (clockService.stop(game)) {
+                        return ResponseUtil.ok();
+                    } else {
+                        return ResponseUtil.badRequest();
+                    }
+                }).orElse(ResponseUtil.badRequest("Klok is niet actief, dus kan je ook niet stoppen"));
     }
 
     @PutMapping("/inc/{gameId}")
@@ -128,35 +112,20 @@ public class ClockResource {
 
         log.debug("Increment game clock {} with {} seconds", gameId, seconds);
 
-        if (gameClockController.isNotRunning()) {
-
-            if (gameId == null || gameId == 0) {
-                return ResponseEntity.badRequest().body(String.format("Game id (%d) is wrong, start 'New Game'.", gameId));
-            }
-
-            return gameService
-                .findGameById(gameId)
-                .map(game -> {
-                    Integer clock = game.getClock();
-                    if (clock < TEN_MINUTES_IN_SECONDS) {
-
-                        if (game.getGameType() == GameType.BASKET_KIDS && clock >= FOUR_MINUTES) {
-                            return ResponseEntity.badRequest().body("Can't be higher than 4 min. for kids basket");
-                        }
-
-                        clock += seconds;
-                        game.setClock(clock);
-                        gameService.update(game);
-
-                        gameClockController.setSeconds(clock);
-                        return ResponseEntity.ok().build();
-                    } else {
-                        return ResponseEntity.badRequest().body("Can't be higher than 10 min.");
-                    }
-                }).orElse(ResponseEntity.badRequest().body("Game niet gevonden"));
+        if (gameId == null || gameId == 0) {
+            return ResponseEntity.badRequest().body(String.format("Game id (%d) is wrong, start 'New Game'.", gameId));
         }
 
-        return ResponseEntity.badRequest().body("Not allowed, clock is still running");
+        return gameService
+            .findGameById(gameId)
+            .map(game -> {
+                if (clockService.incrementClock(game, seconds)) {
+                    return ResponseUtil.ok();
+                } else {
+                    return ResponseUtil.badRequest();
+                }
+            })
+            .orElse(ResponseEntity.badRequest().body("Game niet gevonden"));
     }
 
     @PutMapping("/dec/{gameId}")
@@ -164,31 +133,19 @@ public class ClockResource {
             @PathVariable("gameId") Long gameId,
             @RequestParam("seconds") @DefaultValue("1") int seconds) {
 
-        if (gameClockController.isNotRunning()) {
-
-            if (gameId == null || gameId == 0) {
-                String msg = String.format("Wedstrijd ID (%d) is verkeerd, start 'New Game'.", gameId);
-                return ResponseEntity.badRequest().body(msg);
-            }
-
-            return gameService
-                .findGameById(gameId)
-                .map(game -> {
-                    Integer clock = game.getClock();
-                    if (clock - seconds >= 0) {
-                        clock -= seconds;
-                        game.setClock(clock);
-                        gameService.update(game);
-
-                        gameClockController.setSeconds(clock);
-                        return ResponseEntity.ok().build();
-                    } else {
-                        return ResponseEntity.badRequest().body("Can't have a negative clock");
-                    }
-                })
-                .orElse(ResponseEntity.badRequest().body("Game niet gevonden"));
-        } else {
-            return ResponseEntity.badRequest().body("Not allowed, clock is still running");
+        if (gameId == null || gameId == 0) {
+            return ResponseEntity.badRequest().body(String.format("Game id (%d) is wrong, start 'New Game'.", gameId));
         }
+
+        return gameService
+            .findGameById(gameId)
+            .map(game -> {
+                if (clockService.decrementClock(game, seconds)) {
+                    return ResponseUtil.ok();
+                } else {
+                    return ResponseUtil.badRequest();
+                }
+            })
+            .orElse(ResponseEntity.badRequest().body("Game niet gevonden"));
     }
 }
